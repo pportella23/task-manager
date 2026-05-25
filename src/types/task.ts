c8b98812bdb42157b00ed9cfef5a42570738cce4
Pedro Portella <pportella23@gmail.com>
2026-05-25 19:25:04 -0300

    feat: add TaskAction discriminated to task types

--- src/types/task.ts
+++ src/types/task.ts
@@ -6,3 +6,8 @@ export type Task = {
   createdAt: number;
 }
 export type CreateTask = Omit<Task, 'id' | 'status' | 'createdAt'>;
+export type TaskAction =
+  | { type: 'ADD_TASK'; payload: Task }
+  | { type: 'DELETE_TASK'; id: string }
+  | { type: 'UPDATE_STATUS'; id: string; status: TaskStatus }
+  | { type: 'REORDER_TASK'; dragId: string; dropId: string }
